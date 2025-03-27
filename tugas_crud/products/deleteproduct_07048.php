08d2e464decd874d7070e402e30decc6e14047be
Laela Sugi Nurfatekhah <149172310+lelasgii@users.noreply.github.com>
2025-03-27 13:03:12 +0700

    Update deleteproduct_07048.php

--- tugas_crud/products/deleteproduct_07048.php
+++ tugas_crud/products/deleteproduct_07048.php
@@ -1,17 +1,10 @@
 <?php
-include 'config_07048.php';
+include '../config_07048.php';
+$id = intval($_GET['id']);
 
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
-    $query = "DELETE FROM products WHERE id = ?";
-    $stmt = $conn->prepare($query);
-    $stmt->bind_param("i", $id);
-    
-    if ($stmt->execute()) {
-        header("Location: tabelproducts_07048.php");
-        exit();
-    } else {
-        echo "Gagal menghapus produk.";
-    }
-}
+$stmt = $conn->prepare("DELETE FROM products WHERE id=?");
+$stmt->bind_param("i", $id);
+$stmt->execute();
+
+header("Location: tabelproducts_07048.php");
 ?>
