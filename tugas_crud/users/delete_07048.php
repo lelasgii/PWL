0917164899e1b482f0f4e210f9966d22ccb1f3aa
Laela Sugi Nurfatekhah <149172310+lelasgii@users.noreply.github.com>
2025-03-27 13:01:08 +0700

    Update delete_07048.php

--- tugas_crud/users/delete_07048.php
+++ tugas_crud/users/delete_07048.php
@@ -1,17 +1,6 @@
 <?php
-include 'config_07048.php';
-
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
-    $query = "DELETE FROM users WHERE id = ?";
-    $stmt = $conn->prepare($query);
-    $stmt->bind_param("i", $id);
-    
-    if ($stmt->execute()) {
-        header("Location: tbluser_07048.php");
-        exit();
-    } else {
-        echo "Gagal menghapus pengguna.";
-    }
-}
+include '../config_07048.php';
+$id = $_GET['id'];
+$conn->query("DELETE FROM users WHERE id=$id");
+header("Location: tblusers.php");
 ?>
