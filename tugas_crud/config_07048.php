33a0fa05ddcb13e4a3e1d8fed919891e666c9897
Laela Sugi Nurfatekhah <149172310+lelasgii@users.noreply.github.com>
2025-03-27 13:00:07 +0700

    Update config_07048.php

--- tugas_crud/config_07048.php
+++ tugas_crud/config_07048.php
@@ -1,13 +1,11 @@
 <?php
-$host = "localhost"; 
-$user = "root"; 
-$pass = ""; 
-$dbname = "crud_db"; 
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$dbname = 'crud_db';
 
-// Membuat koneksi ke database
 $conn = new mysqli($host, $user, $pass, $dbname);
 
-// Cek apakah koneksi berhasil
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
 }
