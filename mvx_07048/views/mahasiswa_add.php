8d10a0a4633ed774e46cc22d259aa3df1274e69b
Laela Sugi Nurfatekhah <149172310+lelasgii@users.noreply.github.com>
2025-06-05 08:51:44 +0700

    Create mahasiswa_add.php

---
+++ mvx_07048/views/mahasiswa_add.php
@@ -0,0 +1,7 @@
+<form method="POST" action="index.php?page=store">
+    <input name="nim" placeholder="NIM"><br>
+    <input name="nama" placeholder="Nama"><br>
+    <input name="alamat" placeholder="Alamat"><br>
+    <input name="no_hp" placeholder="No HP"><br>
+    <button type="submit">Simpan</button>
+</form>
